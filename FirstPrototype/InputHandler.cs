c11d7d51a43769a90dafa0b4af90cc07636d57b0
Sarvarian <Sarvarian@Hotmail.com>
2023-02-04 22:08:51 +0330

    I think we're done with Input Handler

--- FirstPrototype/InputHandler.cs
+++ FirstPrototype/InputHandler.cs
@@ -1,11 +1,31 @@
-﻿using Godot;
+﻿using System;
+using Godot;
 
 namespace FirstPrototype;
 
 public class InputHandler
 {
+    public event Action MoveRightSignal;
+    public event Action MoveLeftSignal;
+    public event Action AttackSignal;
+    
     public void NewKeyInput(InputEventKey key)
     {
-        
+        if (key.IsActionPressed(moveRight_))
+        {
+            MoveRightSignal?.Invoke();
+        }
+        else if (key.IsActionPressed(moveLeft_))
+        {
+            MoveLeftSignal?.Invoke();
+        }
+        else if (key.IsActionPressed(attack_))
+        {
+            AttackSignal?.Invoke();
+        }
     }
+    
+    private readonly StringName moveRight_ = "move_right";
+    private readonly StringName moveLeft_ = "move_left";
+    private readonly StringName attack_ = "attack";
 }
