197b37c0a1715f56590b9d34de2c590af6d22944
Sarvarian <Sarvarian@Hotmail.com>
2023-02-04 19:05:25 +0330

    put a character as player in the scene by code

--- FirstPrototype/Main.cs
+++ FirstPrototype/Main.cs
@@ -1,13 +1,28 @@
+using FirstPrototype.entities;
+using FirstPrototype.extensions;
 using Godot;
 
 namespace FirstPrototype;
 
 public partial class Main : Node2D
 {
+    [Export] private PackedScene characterScene_;
+    
     public override void _Ready()
     {
-        GD.Print(CoreGame.TestClass.TestString());
-
+        this.AssertFiledSet(nameof(characterScene_));
+        
+        InstantiatePlayer();
     }
 
+    private readonly CoreGame.World world_ = new();
+    private Character player_;
+
+    private void InstantiatePlayer()
+    {
+        player_ = characterScene_.Instantiate<Character>();
+        AddChild(player_);
+        player_.Position = new Vector2(50, 50);
+    }
+    
 }
--- FirstPrototype/entities/Character.cs
+++ FirstPrototype/entities/Character.cs
@@ -6,13 +6,13 @@ namespace FirstPrototype.entities;
 public partial class Character : Node2D
 {
     [Export] private Sprite2D sprite2D_;
-    
+
     public override void _Ready()
     {
         this.AssertFiledSet(nameof(sprite2D_));
     }
 
-    public override void _Process(double delta)
+    public override void _PhysicsProcess(double delta)
     {
     }
 }
