feebb5ed4ceed9d34dd7b6f9b66c06370741e52e
Sarvarian <Sarvarian@Hotmail.com>
2023-02-06 23:15:23 +0330

    Grid2D

---
+++ SecondPrototype/aban/Grid2D.cs
@@ -0,0 +1,8 @@
+﻿using Godot;
+
+namespace SecondPrototype.aban;
+
+public readonly record struct Grid2D(Vector2I CellSize)
+{
+
+}
---
+++ SecondPrototypeCSharp/UnitTest/TestGrid2D.cs
@@ -0,0 +1,17 @@
+﻿using Godot;
+using SecondPrototype.aban;
+
+namespace UnitTest;
+
+public class TestGrid2D : ClassTestBase
+{
+    [Fact]
+    public void CellSizeInitializedProperly()
+    {
+        Assert.Equal(CellSize, grid2D_.CellSize);
+    }
+
+    private static readonly Vector2I CellSize = RandomVector2I(10, 20, 40, 50);
+    private readonly Grid2D grid2D_ = new(CellSize);
+
+}
