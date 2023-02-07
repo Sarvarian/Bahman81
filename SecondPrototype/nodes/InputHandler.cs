4516df2abfd48f92d4c00f6f6b3d233afb74fe5a
Sarvarian <Sarvarian@Hotmail.com>
2023-02-07 20:55:02 +0330

    bring input handler from first prototype

--- SecondPrototype/nodes/InputHandler.cs
+++ SecondPrototype/nodes/InputHandler.cs
@@ -1,4 +1,5 @@
 ﻿using Godot;
+using System;
 
 namespace SecondPrototype.nodes;
 
@@ -11,9 +12,41 @@ public partial class InputHandler : Node
         return node;
     }
 
+    public event Action? MoveRightSignal;
+    public event Action? MoveLeftSignal;
+    public event Action? AttackSignal;
+    public event Action? ImplantEntitySignal;
+    public event Action? RemoveEntitySignal;
+
     public override void _UnhandledInput(InputEvent @event)
     {
         base._UnhandledInput(@event);
-
+        if (@event.IsActionPressed(MoveRight))
+        {
+            MoveRightSignal?.Invoke();
+        }
+        else if (@event.IsActionPressed(MoveLeft))
+        {
+            MoveLeftSignal?.Invoke();
+        }
+        else if (@event.IsActionPressed(Attack))
+        {
+            AttackSignal?.Invoke();
+        }
+        else if (@event.IsActionPressed(ImplantEntity))
+        {
+            ImplantEntitySignal?.Invoke();
+        }
+        else if (@event.IsActionPressed(RemoveEntity))
+        {
+            RemoveEntitySignal?.Invoke();
+        }
     }
+
+    private static readonly StringName MoveRight = "move_right";
+    private static readonly StringName MoveLeft = "move_left";
+    private static readonly StringName Attack = "attack";
+    private static readonly StringName ImplantEntity = "implant_entity";
+    private static readonly StringName RemoveEntity = "remove_entity";
+
 }
