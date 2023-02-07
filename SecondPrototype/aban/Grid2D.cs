72cb1f0c983b46f92c09d7ff6fcf7719ebd9add4
Sarvarian <Sarvarian@Hotmail.com>
2023-02-07 06:42:09 +0330

    Grid New Cell Size

--- SecondPrototype/aban/Grid2D.cs
+++ SecondPrototype/aban/Grid2D.cs
@@ -1,13 +1,24 @@
 ﻿using Godot;
+using System;
 
 namespace SecondPrototype.aban;
 
 public class Grid2D
 {
-    public Vector2I CellSize { get; }
+    public Vector2I CellSize { get; private set; }
+    public Action? CellSizeUpdatedSignal;
 
     public Grid2D(Vector2I cellSize)
     {
         CellSize = cellSize;
+    }
+
+    public void NewCellSize(Vector2I newSize)
+    {
+        if (CellSize != newSize)
+        {
+            CellSize = newSize;
+            CellSizeUpdatedSignal?.Invoke();
+        }
     }
 }
--- SecondPrototypeCSharp/UnitTest/TestGrid2D.cs
+++ SecondPrototypeCSharp/UnitTest/TestGrid2D.cs
@@ -8,10 +8,47 @@ public class TestGrid2D : ClassTestBase
     [Fact]
     public void CellSizeInitializedProperly()
     {
-        Assert.Equal(CellSize, grid2D_.CellSize);
+        Assert.Equal(InitCellSize, grid_.CellSize);
     }
 
-    private static readonly Vector2I CellSize = RandomVector2I(10, 20, 40, 50);
-    private readonly Grid2D grid2D_ = new(CellSize);
+    [Fact]
+    public void NewCellSizeTest()
+    {
+        Assert.Equal(InitCellSize, grid_.CellSize);
+        grid_.NewCellSize(NewCellSize);
+        Assert.Equal(NewCellSize, grid_.CellSize);
+    }
+
+    [Fact]
+    public void NewCellSizeSignalTest()
+    {
+        var counter = 0;
+        grid_.CellSizeUpdatedSignal += () =>
+        {
+            Assert.Equal(NewCellSize, grid_.CellSize);
+            counter += 1;
+        };
+
+        // Grid cell size has initial value.
+        Assert.Equal(InitCellSize, grid_.CellSize);
+
+        // If we give the same value to grid cell size,
+        // it does not send signal and nothing happens.
+        grid_.NewCellSize(InitCellSize);
+        Assert.Equal(0, counter);
+        Assert.Equal(InitCellSize, grid_.CellSize);
+
+        // When we give a new value to grid cell size,
+        // then size will change and signal will call.
+        grid_.NewCellSize(NewCellSize);
+        Assert.Equal(1, counter);
+        Assert.Equal(NewCellSize, grid_.CellSize);
+    }
+
+
+
+    private static readonly Vector2I InitCellSize = RandomVector2I(10, 20, 40, 50);
+    private static readonly Vector2I NewCellSize = RandomVector2I(70, 80, 100, 110);
+    private readonly Grid2D grid_ = new(InitCellSize);
 
 }
