16bc8124c0cdbc9fdbba8aff743b6669a08013af
Sarvarian <Sarvarian@Hotmail.com>
2023-02-07 19:52:52 +0330

    bring character from first prototype

--- SecondPrototype/aban/entities/Character.cs
+++ SecondPrototype/aban/entities/Character.cs
@@ -1,8 +1,49 @@
-﻿namespace SecondPrototype.aban.entities;
+﻿using System;
+
+namespace SecondPrototype.aban.entities;
 
 public class Character : Entity
 {
+    public Character()
+    {
+        restAction_ = Rest;
+        moveRightAction_ = MoveRight;
+        moveLeftAction_ = MoveLeft;
+        nextMove_ = restAction_;
+    }
+
     public override void Tick()
     {
+        nextMove_();
+        nextMove_ = restAction_;
+    }
+
+    public void SetToMoveRight()
+    {
+        nextMove_ = moveRightAction_;
+    }
+
+    public void SetToMoveLeft()
+    {
+        nextMove_ = moveLeftAction_;
+    }
+
+    private readonly Action restAction_;
+    private readonly Action moveRightAction_;
+    private readonly Action moveLeftAction_;
+    private Action nextMove_;
+
+    private void Rest()
+    {
+    }
+
+    private void MoveRight()
+    {
+        Location += 1;
+    }
+
+    private void MoveLeft()
+    {
+        Location -= 1;
     }
 }
