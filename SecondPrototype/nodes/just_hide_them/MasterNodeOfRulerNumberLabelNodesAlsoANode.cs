ce2d26327a09282d947bf5bcf6eaa7bd10890b91
Sarvarian <Sarvarian@Hotmail.com>
2023-02-08 03:34:29 +0330

    Create MasterNodeOfRulerNumberLabelNodesAlsoANode.cs

---
+++ SecondPrototype/nodes/just_hide_them/MasterNodeOfRulerNumberLabelNodesAlsoANode.cs
@@ -0,0 +1,33 @@
+﻿using Godot;
+
+namespace SecondPrototype.nodes.just_hide_them;
+
+public partial class MasterNodeOfRulerNumberLabelNodesAlsoANode : Control
+{
+    public static MasterNodeOfRulerNumberLabelNodesAlsoANode Instantiate(
+        Node parent,
+        aban.Grid2D grid,
+        CameraNode camera
+    )
+    {
+        var node = new MasterNodeOfRulerNumberLabelNodesAlsoANode(grid, camera);
+        parent.AddChild(node);
+        return node;
+    }
+
+    private MasterNodeOfRulerNumberLabelNodesAlsoANode(aban.Grid2D grid, CameraNode camera)
+    {
+        grid_ = grid;
+        camera_ = camera;
+        ResetLabels();
+    }
+
+    private readonly aban.Grid2D grid_;
+    private readonly CameraNode camera_;
+
+    private void ResetLabels()
+    {
+        // TODO: To be implemented!
+    }
+
+}
