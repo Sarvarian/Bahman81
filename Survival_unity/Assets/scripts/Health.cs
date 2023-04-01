1b2e10fdfc0c0b166518f64f61548d21a22e8850
Sarvarian <Sarvarian@Hotmail.com>
2023-04-01 17:35:58 +0430

    Health

---
+++ Survival_unity/Assets/scripts/Health.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+	public int Value
+	{
+		get
+		{
+			return Get;
+		}
+		set
+		{
+			Set(value);
+		}
+	}
+
+	public int Get
+	{
+		get
+		{
+			return value_;
+		}
+	}
+
+	public void Set(int newValue)
+	{
+		value_ = newValue;
+	}
+
+	public void Decrement(int amount = 1)
+	{
+		Set(value_ - amount);
+	}
+
+	private int value_ = 10;
+
+}
