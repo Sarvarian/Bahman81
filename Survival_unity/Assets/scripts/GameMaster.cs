19d08a8194993bfe0969ff846cac79fc22cb5f56
Sarvarian <Sarvarian@Hotmail.com>
2023-04-02 21:00:10 +0430

    !!!

--- Survival_unity/Assets/scripts/GameMaster.cs
+++ Survival_unity/Assets/scripts/GameMaster.cs
@@ -4,11 +4,14 @@
 public class GameMaster : MonoBehaviour
 {
 	[SerializeField] private GameObject characterPrefab;
+	[SerializeField] private GameObject barPrefab;
 
 	private void Start ()
 	{
 		var player = Instantiate(characterPrefab);
 		player.GetComponent<Location>().Set(0);
 		player.AddComponent<PlayerController>();
+
+		var bar = Instantiate(barPrefab, player.transform, true);
 	}
 }
